870561cb9380d097ef3dc8853704af4250baaeed
Nabil Jahir <jahir.nabil@gmail.com>
2019-04-11 23:43:20 +0100

    sending mail done

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,26 +9,38 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/contact", name="default", methods={"POST"})
+     * @Route("/contact", name="default")
      */
     public function index(Request $request,\Swift_Mailer $mailer)
     {
-        $name = $request->request->get('name');
-        $lastname = $request->request->get('lastname');
-        $email = $request->request->get('email');
-        $messageContent = $request->request->get('message');
+        
+        $mail = json_decode($request->getContent());
+        if( $mail->name && $mail->lastname && $mail->phone && $mail->email && $mail->message){
+            $name = $mail->name;
+            $lastname = $mail->lastname;
+            $email = $mail->email;
+            $messageContent = $mail->message;
+            $phone = $mail->phone;
 
-        $body ="Name : $name \r\n Lastname : $lastname \r\n Email : $email \r\n Messahe :  $messageContent";
-        $message = (new \Swift_Message('Enquiry from gotgroup'))
-            ->setFrom( 'nabil@gotgroup.co.uk')
-            ->setTo( 'enquiries@gotgroup.co.uk')
-            ->setBody( $body, 'text/plain');
+            $body = "Name : $name \r\n Lastname : $lastname \r\n Phone : $phone \r\n Email : $email \r\n Messahe :  $messageContent";
+            $message = (new \Swift_Message('Hello Email'))
+                ->setFrom('nabil@gotgroup.co.uk')
+                ->setTo('jahir.nabil.wrk@gmail.com')
+                ->setBody($body, 'text/plain');
 
-        $mailer->send($message);
+            $mailer->send($message);
 
-        return $this->json([
-            'status' => 'success',
-            'message' => 'Message have been sent.',
-        ]);
+            return $this->json([
+                'status' => 'success',
+                'message' => 'Message have been sent.',
+            ]);
+        }
+        else{
+            return $this->json([
+                'status' => 'error',
+                'message' => 'Required fields missing!',
+            ]);
+        }
+        
     }
 }
